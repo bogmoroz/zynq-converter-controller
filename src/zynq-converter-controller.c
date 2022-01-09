4d16fd64265b83f228bb883f0be2bf78f4baa010
Bogdan Moroz <bogdan.moroz@observis.fi>
2022-01-09 19:05:30 +0200

    Basic code formatting

--- src/zynq-converter-controller.c
+++ src/zynq-converter-controller.c
@@ -3,53 +3,56 @@
 #include "platform.h"
 #include <xil_printf.h>
 
-float convert (float u) {
-unsigned int i,j;
-static const float A[6][6] = {
-		{0.9652,-0.0172,0.0057,-0.0058,0.0052,-0.0251}, /* row 1 */
+float convert(float u)
+{
+	unsigned int i, j;
+	static const float A[6][6] = {
+		{0.9652, -0.0172, 0.0057, -0.0058, 0.0052, -0.0251}, /* row 1 */
 
-		{0.7732,0.1252,0.2315,0.07,0.1282,0.7754}, /* row 2 */
+		{0.7732, 0.1252, 0.2315, 0.07, 0.1282, 0.7754}, /* row 2 */
 
-		{0.8278,-0.7522,-0.0956,0.3299,-0.4855,0.3915}, /* row 3 */
+		{0.8278, -0.7522, -0.0956, 0.3299, -0.4855, 0.3915}, /* row 3 */
 
-		{0.9948,0.2655,-0.3848,0.4212,0.3927,0.2899}, /* row 4 */
+		{0.9948, 0.2655, -0.3848, 0.4212, 0.3927, 0.2899}, /* row 4 */
 
-		{0.7648,-0.4165,-0.4855,-0.3366,-0.0986,0.7281}, /* row 5 */
+		{0.7648, -0.4165, -0.4855, -0.3366, -0.0986, 0.7281}, /* row 5 */
 
-		{1.1056,0.7587,0.1179,0.0748,-0.2192,0.1491} /* row 6 */
+		{1.1056, 0.7587, 0.1179, 0.0748, -0.2192, 0.1491} /* row 6 */
 
-};
+	};
 
-static const float B[6] = {
+	static const float B[6] = {
 		0.0471,
 		0.0377,
 		0.0404,
 		0.0485,
 		0.0373,
 		0.0539
-};
+	};
 
-static float states[6] = {0.0,0.0,0.0,0.0,0.0,0.0};
-static float oldstates[6] = {0.0,0.0,0.0,0.0,0.0,0.0};
+	static float states[6] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+	static float oldstates[6] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
 
-for (i=0; i<6;i++){
-	states[i] = 0.0;
-	for (j=0;j<6;j++){
-		states[i] = states[i] + A[i][j]*oldstates[j];
+	for (i = 0; i < 6; i++)
+	{
+		states[i] = 0.0;
+		for (j = 0; j < 6; j++)
+		{
+			states[i] = states[i] + A[i][j] * oldstates[j];
+		}
+		states[i] = states[i] + B[i] * u;
 	}
-	states[i] = states[i] + B[i] * u;
-}
-for (i=0;i<6;i++){
-	oldstates[i] = states[i];
-}
 
-return states[5];
+	for (i = 0; i < 6; i++)
+	{
+		oldstates[i] = states[i];
+	}
 
+	return states[5];
 };
-
 
 int main()
 {
-    xil_printf("Hello World\n\r");
-    return 0;
+	xil_printf("Hello World\n\r");
+	return 0;
 }
